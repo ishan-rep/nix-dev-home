54b30bacfe5627182e5a54398f290a0b3249b973
Ishan Sharma <ishan.s@juspay.in>
2024-09-19 11:21:06 +0530

    Initial commit with modified files

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  nixConfig = {
+    extra-substituters = "https://om.cachix.org";
+    extra-trusted-public-keys = "om.cachix.org-1:ifal/RLZJKN4sbpScyPGqJ2+appCslzu7ZZF/C01f2Q=";
+  };
+
+  description = "A home-manager template providing useful tools & settings for Nix-based development";
+
+  inputs = {
+    # Principle inputs (updated by `nix run .#update`)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixos-flake.url = "github:srid/nixos-flake";
+    systems.url = "github:nix-systems/default";
+
+    # Software inputs
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    omnix.url = "github:juspay/omnix";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.nixos-flake.flakeModule
+        ./nix/modules/flake-parts/template.nix
+        ./nix/modules/flake-parts/toplevel.nix
+      ];
+
+      flake.nix-dev-home.username = "ishansharma";
+
+      perSystem = { self', pkgs, ... }: {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          name = "nix-dev-home";
+          nativeBuildInputs = with pkgs; [ just nixd ];
+        };
+      };
+    };
+}
